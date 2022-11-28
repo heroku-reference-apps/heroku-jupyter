20f4dba482d795339e39c73bd038d5cf5c86407e
zhengshunze <77151276+zhengshunze@users.noreply.github.com>
2022-11-28 19:46:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-import subprocess
-
 from flask import Flask
 app = Flask(__name__)
 
-def run_command(command):
-    return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read()
-
-@app.route('/<command>')
-def command_server(command):
-    return run_command(command)
+@app.route("/index")
+def hello():
+    return "Hello, World!"
